9608a84febe70d1660d9752df86a62a23be01b97
dhaile <dawityhaile@gmail.com>
2018-10-19 12:58:54 -0700

    initial project

---
+++ almaz/Views/dashboard/index.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace almaz.Views.dashboard
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class index
+    {
+        private readonly RequestDelegate _next;
+
+        public index(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public Task Invoke(HttpContext httpContext)
+        {
+
+            return _next(httpContext);
+        }
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class indexExtensions
+    {
+        public static IApplicationBuilder UseMiddlewareClassTemplate(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<index>();
+        }
+    }
+}
